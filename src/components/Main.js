a17eac0fdf436ca86e6a89d4f4c98461da09a296
Ganesh Balaji <ganeshkbalaji@ymail.com>
2016-02-06 00:05:26 -0800

    cleaned up directory and started new project

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,6 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice"></div>
       </div>
     );
